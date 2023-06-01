a9d9922656c610cbbdc26bc1886ca8fea5f3efb3
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2023-06-01 16:02:32 -0400

    'types'

---
+++ src/@types/alltypes.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-router';
+declare module 'otp-input-react';
+declare module 'khalti-checkout-web';
+declare module 'react-datepicker';
+declare module 'react-responsive';
+declare module 'react-slider';
+declare module 'react-select';
+declare module 'react-table';
+declare module 'react-cssfx-loading';
+declare module 'react-rating-stars-component';
+declare module '@headlessui/react';
+declare module 'yup';
