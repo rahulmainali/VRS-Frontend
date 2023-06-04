c8130338b60d03030236de81bda62b9056f1d7f0
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2023-06-04 19:06:49 -0400

    hooks.ts

---
+++ src/app/hooks.ts
@@ -0,0 +1,5 @@
+// the whole purpose is to add type in the functions provided by react-redux 
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux' 
+import {RootState, AppDispatch} from './store'
+export const useAppDispatch = ()=> useDispatch<AppDispatch>()
+export const useAppSelector : TypedUseSelectorHook<RootState> = useSelector;
