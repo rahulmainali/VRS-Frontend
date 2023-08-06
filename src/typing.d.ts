4961d335be3d10a4b7c64fbe9c0e31a4a13d1f4d
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2023-08-06 12:50:13 -0400

    'explicitTypes'

---
+++ src/typing.d.ts
@@ -0,0 +1,9 @@
+declare module '@ckeditor/ckeditor5-build-classic' {
+    const ClassicEditorBuild: any;
+
+    export = ClassicEditorBuild;
+}
+declare module '@otp-input-react' {
+    const OtpInputReact: any; 
+    export = OtpInputReact;
+}
